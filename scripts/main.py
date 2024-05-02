2909a047f4560fdf29623fc954550b66af7bdf3f
Davi Alexandre <deltreertl@gmail.com>
2024-05-01 21:28:10 -0300

    links

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 from markdownify import markdownify as md
 from bs4 import BeautifulSoup
+from urllib.parse import unquote
 import shutil
+import re
+from re import Match
 
 MD_DIR = Path("md")
 PUBLIC = Path("public")
@@ -11,7 +14,9 @@ if PUBLIC.exists():
 if MD_DIR.exists():
     shutil.rmtree(MD_DIR)
 
-IMG_EXT = list(map(lambda e: f".{e}", ["png", "jpg", "jpeg", "gif", "webp", "svg"]))
+IMG_SUFFIXES = list(
+    map(lambda e: f".{e}", ["png", "jpg", "jpeg", "gif", "webp", "svg"])
+)
 IMG_DIR, FILES_DIR = PUBLIC / "images", PUBLIC / "file"
 for p in [PUBLIC, IMG_DIR, FILES_DIR]:
     p.mkdir(exist_ok=True)
@@ -58,12 +63,30 @@ def move_dir_content():
     for file in MD_DIR.rglob("*"):
         if file.is_dir() or file.suffix == ".md":
             continue
-        target_dir = IMG_DIR if file.suffix in IMG_EXT else FILES_DIR
+        target_dir = IMG_DIR if file.suffix in IMG_SUFFIXES else FILES_DIR
         target_file = target_dir / file.name
         if target_file.exists():
-            target_file = target_dir / file.parent.parent.name
+            target_file = target_dir / (file.parent.parent.name + file.suffix)
         shutil.move(file, target_file)
 
 
+def update_links():
+    XP = r"!\[(?P<desc>.*?)\]\((?P<link>.+?)(?:[ ]\"(?P<alt>.+?)\")?\)"
+    for md in MD_DIR.rglob("*.md"):
+        text = md.read_text("utf-8")
+
+        def parse(x: Match):
+            d = x.groupdict()
+            desc, link, alt = d["desc"], d["link"], d["alt"]
+            path = md.parent / unquote(link)
+            path = Path("/images") / path.name
+            return f'![{desc}]({path} "{alt}")'
+
+        if re.search(XP, text):
+            text = re.sub(XP, parse, text)
+        md.write_text(text, "utf-8")
+
+
 to_md()
+update_links()
 populate_public()
