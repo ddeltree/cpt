88855a47ce422b756951c64a3733e921a069e549
Davi Alexandre <deltreertl@gmail.com>
2024-05-15 21:08:26 -0300

    atualizar classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: "'Inter Variable', sans-serif",
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
