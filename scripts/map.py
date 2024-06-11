1875bd2e41eff08e1fcc967e0e5ad85f71b5203b
Davi Alexandre <deltreertl@gmail.com>
2024-06-10 22:52:44 -0300

    add iter_tree

--- scripts/map.py
+++ scripts/map.py
@@ -1,27 +1,56 @@
 from pathlib import Path
+import yaml
 
 
-def to_str(
-    directory: Path,
-    subdir: Path,
-    padding: int,
+class Subdirectories(type(Path())):
+    def __init__(self, parent: Path):
+        self.ref = parent
+        self.state = parent.iterdir()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.state)
+
+
+def iter_directories():
+    iter_tree(
+        MD,
+        to_iter=lambda x: Subdirectories(x),
+        should_stack=lambda x: x.is_dir(),
+        on_pop=lambda x: add_entry([y.ref for y in x]),
+    )
+
+
+def iter_tree(
+    root, to_iter=lambda x: iter(x), should_stack=lambda: False, on_pop=lambda: ...
 ):
-    return "\t" * padding + str(subdir.relative_to(directory)) + "\n"
+    stack = [to_iter(root)]
+    while stack:
+        node = stack[-1]
+        child = next(node, None)
+        if child and should_stack(child):
+            stack.append(to_iter(child))
+        elif not child:
+            on_pop(stack)
+            stack.pop()
 
 
-text = ""
-directories = [(Path("md"), Path("md").iterdir())]
+def add_entry(dir_stack):
+    ref = YAML
+    for directory in dir_stack:
+        key = str(directory.relative_to(MD).name)
+        if not key:
+            continue
+        ref[key] = ref.get(key, dict())
+        ref = ref[key]
 
-while directories:
-    ((directory, iterator)) = directories[-1]
-    subdir = next((iterator), None)
-    while subdir:
-        if subdir.is_dir():
-            text += to_str(directory, subdir, len(directories) - 1)
-            directories.append((subdir, subdir.iterdir()))
-            break
-        subdir = next((iterator), None)
-    else:
-        directories.pop()
 
-Path("map.txt").write_text(text, "utf-8")
+YAML = dict()
+MD = Path("md")
+
+iter_directories()
+
+
+Path("sitemap.yaml").write_text(yaml.dump(YAML), "utf-8")
