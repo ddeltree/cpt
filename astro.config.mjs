e479a552fa8cfc4d997063e0fee6f334588ca44b
Davi Alexandre <deltreertl@gmail.com>
2024-07-02 18:22:15 -0300

    add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
     mdx({
       optimize: true,
     }),
+    icon(),
   ],
 })
