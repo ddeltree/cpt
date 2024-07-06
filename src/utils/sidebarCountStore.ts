820ab09497b43e936085ad6b7e1de9362a5469f3
Davi Alexandre <deltreertl@gmail.com>
2024-07-06 19:43:59 -0300

    refactor: remove css `:target` logic from the sidebar

--- src/utils/sidebarCountStore.ts
+++ src/utils/sidebarCountStore.ts
@@ -1,17 +1,6 @@
 let collapsibleCount = 0
-let sidebarLinkCount = 0
 
 export function getCountId() {
   collapsibleCount += 1
   return `collapsible-${collapsibleCount}` as const
-}
-
-export function getSidebarLinkCountId() {
-  sidebarLinkCount += 1
-  return `link-${sidebarLinkCount}` as const
-}
-
-export function resetCounters() {
-  collapsibleCount = 0
-  sidebarLinkCount = 0
 }
