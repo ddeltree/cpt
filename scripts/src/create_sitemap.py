364d96b441918ef3f1f965827761d8b186817244
Davi Alexandre <deltreertl@gmail.com>
2024-07-25 23:17:50 -0300

    [sitemap] update

--- scripts/src/create_sitemap.py
+++ scripts/src/create_sitemap.py
@@ -4,52 +4,6 @@ from typing import Tuple
 from utils.globals import MD_DIR, REDIRECTS_CSV_PATH, ROOT_URL, SITEMAP_PATH, PUBLIC_DIR
 
 
-def iter_tree(
-    root,
-    to_iter=lambda child: iter(child),
-    should_stack=lambda child: False,
-    on_pop=lambda stack: ...,
-):
-    stack = [to_iter(root)]
-    while stack:
-        node = stack[-1]
-        child = next(node, None)
-        if child is not None and should_stack(child):
-            stack.append(to_iter(child))
-        elif not child:
-            on_pop(stack)
-            stack.pop()
-
-
-def make_iterator(entry: Tuple[str, dict, dict] | dict):
-    path, parent, _ = entry if not isinstance(entry, dict) else (None, entry, None)
-    for key, value in parent.items():
-        new_path = f"{path}/{key}" if path else key
-        yield (new_path, value, parent)
-
-
-def should_stack(child: Tuple[str, dict, dict]):
-    path, value, parent = child
-    if len(value.keys()) == 0:
-        last_key = path.split("/")[-1]
-        if path in REDIRECT_ROUTES:
-            path = REDIRECT_URLS[REDIRECT_ROUTES.index(path)]
-        parent[last_key] = path
-    return True
-
-
-def read_redirects():
-    global REDIRECT_ROUTES, REDIRECT_URLS
-    res: list[Tuple[str, str]] = []
-    with REDIRECTS_CSV_PATH.open() as f:
-        reader = csv.reader(f)
-        for route, redirect in reader:
-            route = route.replace(ROOT_URL, "")
-            route = route[1:] if route.startswith("/") else route
-            res.append((route, redirect))
-    REDIRECT_ROUTES, REDIRECT_URLS = zip(*res)
-
-
 REDIRECT_ROUTES, REDIRECT_URLS = None, None
 
 
@@ -83,3 +37,51 @@ def main():
     )
     SITEMAP_PATH.write_text(json.dumps(yml))
     shutil.copy(SITEMAP_PATH, PUBLIC_DIR)
+
+
+def read_redirects():
+    global REDIRECT_ROUTES, REDIRECT_URLS
+    res: list[Tuple[str, str]] = []
+    with REDIRECTS_CSV_PATH.open() as f:
+        reader = csv.reader(f)
+        for route, redirect in reader:
+            route = route.replace(ROOT_URL, "")
+            route = route[1:] if route.startswith("/") else route
+            res.append((route, redirect))
+    REDIRECT_ROUTES, REDIRECT_URLS = zip(*res)
+
+
+def iter_tree(
+    root,
+    to_iter=lambda child: iter(child),
+    should_stack=lambda child: False,
+    on_pop=lambda stack: ...,
+):
+    stack = [to_iter(root)]
+    while stack:
+        node = stack[-1]
+        child = next(node, None)
+        if child is not None and should_stack(child):
+            stack.append(to_iter(child))
+        elif not child:
+            on_pop(stack)
+            stack.pop()
+
+
+def make_iterator(entry: Tuple[str, dict, dict] | dict):
+    path, parent, _ = entry if not isinstance(entry, dict) else (None, entry, None)
+    for key, value in parent.items():
+        new_path = f"{path}/{key}" if path else key
+        yield (new_path, value, parent)
+
+
+def should_stack(child: Tuple[str, dict, dict]):
+    path, value, parent = child
+    if len(value.keys()) == 0:
+        last_key = path.split("/")[-1]
+        if path in REDIRECT_ROUTES:
+            path = REDIRECT_URLS[REDIRECT_ROUTES.index(path)]
+        else:
+            path = "/cpt/" + path
+        parent[last_key] = path
+    return True
