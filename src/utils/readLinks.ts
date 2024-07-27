19f3997c9313a2a9a84a28fe34be3eecd4a35345
Davi Alexandre <deltreertl@gmail.com>
2024-07-26 23:30:00 -0300

    checar se o link existe

---
+++ src/utils/readLinks.ts
@@ -0,0 +1,12 @@
+import fs from 'fs'
+
+const rootURL = 'https://arapiraca.ufal.br/graduacao/ciencia-da-computacao'
+
+const links = fs
+  .readFileSync('scripts/data/links.txt', 'utf-8')
+  .split('\n')
+  .map((x) => x.trim())
+  .filter((x) => x !== '')
+  .map((x) => encodeURI(x))
+  .map((x) => x.replace(rootURL, '/cpt'))
+export default links
